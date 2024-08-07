46240dcbc5a0a42892c61db78e7b50b36d17c4d0
Ahtisham420 <ahtishamqayyum8@gmail.com>
2024-08-08 00:26:20 +0500

    Errors Resolved

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'alreadyloggedin' => \App\Http\Middleware\AlreadyLoggedIn::class,
+        'checkuserloggedin' => \App\Http\Middleware\checkUserLoggedIn::class,
     ];
 }
